fc8ebf52b1b59603da730ab52e389fe12efa4e72
Patrick Koch <Patrick.Koch@aau.at>
2019-05-02 10:03:53 +0200

    added additional plots for appendix

--- evaluation/lib/data.py
+++ evaluation/lib/data.py
@@ -13,19 +13,19 @@ class DataSet:
     y = None
     w = None
     h = None
-    
+
     def printStatistics(self):
         print("*DATASET "+self.name+"*")
         print("#total:      ", str(len(self.y)))
         print("#faulty:     ", str(sum(self.y)))
         print("#non-faulty: ", len(self.y) - sum(self.y))
-        print("%faulty:     ", '{:.1%}'.format(sum(self.y) / len(self.y)))    
+        print("%faulty:     ", '{:.1%}'.format(sum(self.y) / len(self.y)))
 
 # removeDuplicates function: removes duplicates from data
 def removeDuplicates(data_relevant):
     ##simple version
-    #data_unique = np.vstack({tuple(row) for row in data_relevant})  
-    
+    #data_unique = np.vstack({tuple(row) for row in data_relevant})
+
     ##elaborate version, counting number of duplicates per entry type
     d = collections.OrderedDict()
     for a in data_relevant:
@@ -44,27 +44,27 @@ def removeDuplicates(data_relevant):
 # getData function: reads data from CSV and returns DataSet
 def getData(csvFile, name, removeDuplicateEntries = False):
     print("processing dataset "+name+"...")
-    
+
     #print("Reading data of file "+csvFile)
     #d=pd.read_csv(csvFile, sep=';', dtype={'FAULTY':str}, encoding='latin-1')
     df=pd.read_csv(csvFile, sep=';', encoding='latin-1')
     #df=pd.read_csv(csvFile, sep=';', encoding='latin-1')
-    
+
     ##extract headers
     headers = df.columns.values[4:]
     #print(headers)
-    
+
     ##extract relevant data
     data_relevant = df.as_matrix(columns=df.columns[3:])
-    
+
     ##converto true/false/nan to 0/1
     data_relevant[pd.isnull(data_relevant)] = 0
     #data_relevant[data_relevant != 0] = 1
     data_relevant[data_relevant == False] = 0
-    data_relevant[data_relevant == True] = 1   
+    data_relevant[data_relevant == True] = 1
     #print("relevant and converted target & data:")
     #print(data_relevant)
-    
+
     if removeDuplicateEntries:
         ##remove duplicates
         faultyDataBefore = sum(data_relevant[:,0])
@@ -72,7 +72,7 @@ def getData(csvFile, name, removeDuplicateEntries = False):
         data_unique, weights = removeDuplicates(data_relevant)
         faultyDataAfter = sum(data_unique[:,0])
         correctDataAfter = len(data_unique) - faultyDataAfter
-        
+
         print("removed " + str(faultyDataBefore - faultyDataAfter) + " duplicate(s) of " + str(faultyDataBefore) + " faulty entries")
         print("removed " + str(correctDataBefore - correctDataAfter) + " duplicate(s) of " + str(correctDataBefore) + " correct entries")
         print("total entries reduced from ", str(len(data_relevant)), " to ", str(len(data_unique)))
@@ -86,25 +86,25 @@ def getData(csvFile, name, removeDuplicateEntries = False):
     data = data_unique[:,1:]
     #print("unique data entries:")
     #print(data)
-    
+
     ##scale data
     #data = StandardScaler().fit_transform(data)
     #print(data)
-    
+
     ##extract target portion
     target = data_unique[:,0]
     #print("unique target entries:")
     #print(target)
-    
+
     #data = df.as_matrix(columns=df.columns[5:])
     #print(data)
-    
+
     #target = df.as_matrix(['FAULTY'])[:,0]
     #target[pd.isnull(target)] = 0
     #target[target != 0] = 1
     #target = target.astype(int)
     #print_full(target)
-    
+
     dataset = DataSet(name)
     dataset.X = data
     dataset.y = target
@@ -118,7 +118,7 @@ def resample(sampler, dataset):
     X_resampled, y_resampled = sampler.fit_sample(dataset.X, dataset.y)
     #reset weights to ones
     w_resampled = np.ones(len(y_resampled))
-    
+
     dataset_resampled = DataSet(dataset.name+" resampled")
     dataset_resampled.X = X_resampled
     dataset_resampled.y = y_resampled
@@ -128,6 +128,14 @@ def resample(sampler, dataset):
 
 def loadDataset(key, removeDuplicateEntries=False):
 	return getData(DATASET[key]['csv'], DATASET[key]['name'], removeDuplicateEntries)
+
+def combineDatasets(a, b, name):
+    newDataSet = DataSet(name)
+    newDataSet.X = np.concatenate((a.X,b.X), axis=0)
+    newDataSet.y = np.concatenate((a.y,b.y))
+    newDataSet.w = np.concatenate((a.w,b.w))
+    newDataSet.h = a.h
+    return newDataSet
 
 DEFAULT_DATASET_FILENAME = "data/datsets.json"
 
@@ -156,10 +164,10 @@ def exportDatasetJSON(filename = DEFAULT_DATASET_FILENAME):
 	csv_info1_10k   = 'metric_data\Info1_2017-08-01T15-38\cells_10k.csv'
 	csv_info1_20k   = 'metric_data\Info1_2017-08-01T15-38\cells_20k.csv'
 	csv_info1_50k   = 'metric_data\Info1_2017-08-01T15-38\cells_50k.csv'
-	
+
 	csv_enron_journal = 'metric_data\journal_2017-12-07\enron.csv'
 	csv_info1_journal = 'metric_data\journal_2017-12-07\info1.csv'
-	
+
 	dataset = {}
 	dataset["mini"] = {}
 	dataset["mini"]["csv"] = csv_minimal
@@ -176,7 +184,7 @@ def exportDatasetJSON(filename = DEFAULT_DATASET_FILENAME):
 	dataset["info1"] = {}
 	dataset["info1"]["csv"] = csv_info1
 	dataset["info1"]["name"] = "INFO1"
-	
+
 	dataset["enron_100"] = {}
 	dataset["enron_100"]["csv"] = csv_enron_100
 	dataset["enron_100"]["name"] = "ENRON_100"
@@ -198,7 +206,7 @@ def exportDatasetJSON(filename = DEFAULT_DATASET_FILENAME):
 	dataset["enron_10k"] = {}
 	dataset["enron_10k"]["csv"] = csv_enron_10k
 	dataset["enron_10k"]["name"] = "ENRON_10k"
-	
+
 	dataset["info1_100"] = {}
 	dataset["info1_100"]["csv"] = csv_info1_100
 	dataset["info1_100"]["name"] = "INFO1_100"
@@ -231,46 +239,46 @@ def exportDatasetJSON(filename = DEFAULT_DATASET_FILENAME):
 	csv_info1_journal = 'metric_data\journal_2017-12-07\info1.csv'
 	csv_euses_journal = 'metric_data\journal_2017-12-07\euses.csv'
 
-	
+
 	dataset["enron_journal"] = {}
 	dataset["enron_journal"]["csv"] = csv_enron_journal
 	dataset["enron_journal"]["name"] = "ENRON"
 	dataset["info1_journal"] = {}
 	dataset["info1_journal"]["csv"] = csv_info1_journal
-	dataset["info1_journal"]["name"] = "INFO1"	
+	dataset["info1_journal"]["name"] = "INFO1"
 	dataset["euses_journal"] = {}
 	dataset["euses_journal"]["csv"] = csv_euses_journal
-	dataset["euses_journal"]["name"] = "EUSES"	
-	
-	print("writing to ", filename) 
+	dataset["euses_journal"]["name"] = "EUSES"
+
+	print("writing to ", filename)
 	print(dataset)
-	
+
 	if not os.path.exists(os.path.dirname(filename)):
 		try:
 			os.makedirs(os.path.dirname(filename))
 		except OSError as exc: # Guard against race condition
 			if exc.errno != errno.EEXIST:
 				raise
-	
+
 	with open(filename, 'w') as fp:
 		json.dump(dataset, fp)
-		
+
 	print("written to ", filename)
 
 def importDatasetJSON(filename = DEFAULT_DATASET_FILENAME):
 	with open(filename, 'r') as fp:
 		data = json.load(fp)
 	return data
-	
-	
+
+
 def testFunction(filename = DEFAULT_DATASET_FILENAME):
 	print ("do nothing")
-	
+
 #import default dataset if available
 from pathlib import Path
 dataset_file = Path(DEFAULT_DATASET_FILENAME)
 
 if not dataset_file.exists():
 	exportDatasetJSON(DEFAULT_DATASET_FILENAME)
-	
+
 DATASET = importDatasetJSON()
